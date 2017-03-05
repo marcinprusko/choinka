5349b43000d081be38bcf25f83d3d8a64082b2c8
marcinprusko <marcinprusko@gmail.com>
2017-03-05 18:12:54 +0100

    choinka

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+﻿// plik scripts.js
+
+function rysujChoinke(rows) {
+	// wysokosc choinki
+	var star;
+	for (var i = 0 ; i < rows; i++) {
+		
+		// ilosc gwiazdek w rzedzie
+		star = '*';
+		for (var j = 0; j < i * 2 - 1; j++) {
+			star += '*';
+		}
+
+		console.log(star);
+	}
+}
+
+rysujChoinke(5);
